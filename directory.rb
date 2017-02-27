708ffe114b653bab46b6eec269c539acd536463b
Jack Bittiner <jackbittiner@hotmail.com>
2017-02-27 16:49:20 +0000

    Step5. Added Methods

--- directory.rb
+++ directory.rb
@@ -11,9 +11,19 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"]
 
-puts "The students of Villains Academy"
-puts "-------------"
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
-students.each {|student| puts student}
+def print(names)
+  names.each {|name| puts name}
+end
 
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students."
+end
+
+print_header
+print(students)
+print_footer(students)
