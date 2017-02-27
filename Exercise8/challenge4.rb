e6f0b6e66d44e17ffd85931f533fe4d37807415b
Jack Bittiner <jackbittiner@hotmail.com>
2017-02-27 22:38:15 +0000

    Exercise8.4 - Use while instead of each

---
+++ Exercise8/challenge4.rb
@@ -0,0 +1,34 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  i = 0
+  while i < students.count do
+puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+  i +=1
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students."
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
