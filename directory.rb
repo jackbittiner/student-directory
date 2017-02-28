3aa237e5a2ba6f1436e7d8ac2e16aeb017c63cb1
Jack Bittiner <jackbittiner@hotmail.com>
2017-02-28 13:12:05 +0000

    step10 - refactoring

--- directory.rb
+++ directory.rb
@@ -1,44 +1,46 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-  case selection
-  when "1"
-    students = input_students
-  when "2"
-    print_header
-    print(students)
-    print_footer(students)
-  when "9"
-    exit
-  else
-    puts "I don't know what you meant, try again."
-  end
-end
-end
-
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
-  puts "And which cohort are they on?"
-  cohort = gets.chomp
-  cohort.empty? ? cohort = :november : cohort = cohort.downcase.to_sym
-  while !name.empty? && !cohort.empty? do
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} #{plural(students)}"
-    puts "Next student's name:"
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
-    puts "and their cohort?"
-    cohort = gets.chomp
-    cohort.empty? ? cohort = :november : cohort = cohort.to_sym
   end
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -46,18 +48,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-students.each do |student|
-  puts "#{student[:name]}, (#{student[:cohort]} cohort)"
-end
-end
-
-def plural(students)
-  students.count == 1 ? "student" : "students"
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great #{plural(students)}."
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
